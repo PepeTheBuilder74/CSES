2493024356b9f8dc740e0c036b41b1c09bcb45c6
PepeTheBuilder74 <amanghosh74@gmail.com>
2023-07-18 19:31:59 +0530

    Book_shop

--- DP/Book_Shop.cpp
+++ DP/Book_Shop.cpp
@@ -22,6 +22,17 @@ int32_t main() {
     return 0;
 }
 void __solve_testcase(int test_case) {
-    
-
+    int N,X; cin>>N>>X;
+    vector<int>price(N),pages(N); 
+    for(auto &x:price)cin>>x; 
+    for(auto &x:pages)cin>>x;
+    vector<int>dp(X+1,0);
+    for(int i=0;i<N;i++){
+        for(int j=X;j>=0;j--){
+           if(j-price[i]>=0){
+             dp[j]=max(dp[j],dp[j-price[i]]+pages[i]);
+           }
+        }
+    }
+    cout<<dp[X]<<'\n';
 }
