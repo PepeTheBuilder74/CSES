b2d1bd965e59f475a466d785485d82aed3ae4e96
PepeTheBuilder74 <amanghosh74@gmail.com>
2023-07-20 19:27:23 +0530

    Elevator Rides

--- DP/Elevator_Rides.cpp
+++ DP/Elevator_Rides.cpp
@@ -15,12 +15,24 @@ int32_t main() {
     }
     return 0;
 }
-
+const ll INF=1e18+5;
 void __solve_testcase(int test_case) {
    int N,X; cin>>N>>X;
    vector<ll> a(N);
    for(auto &x:a)cin>>x;
-   vector<ll>dp(1<<N,1e9);
-   dp[0]=0;
-   
+   vector<pair<ll,ll>>dp(1<<N,{INF,INF});
+   dp[0]={0,0};
+   for(int mask=1;mask<(1<<N);mask++){
+     for(int i=0;i<N;i++) if(mask&(1<<i)){
+          auto [x,y]=dp[mask^(1<<i)];
+          if(x+a[i]<=X){
+              x+=a[i];
+          }else{
+            x=a[i];
+            y++;
+          }
+          dp[mask]=min(dp[mask],{x,y});
+       }
+   }
+   cout<<dp[(1<<N)-1].second+1<<nl;
 }
